c791b02654d332bbb256b52f2e8336531cb87f12
mrexodia <mr.exodia.tpodt@gmail.com>
2017-01-21 21:40:22 +0100

    release script

---
+++ release.bat
@@ -0,0 +1,5 @@
+rmdir /S /Q release
+mkdir release
+copy DarkSouls3.TextViewer\bin\Release\DarkSouls3.TextViewer.exe release\
+copy DarkSouls3.TextViewer\bin\Release\DarkSouls3.Structures.dll release\
+copy ds3.json release\
